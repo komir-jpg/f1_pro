72ab30d3eeb64324c598a1bd6bf0dfddf114a052
kimor-wsl <mirkoe152@gmail.com>
2026-05-18 22:50:53 +0200

    refactor and lap_data

--- src/hello.c
+++
@@ -1,9 +0,0 @@
-#include "hello.h"
-
-#include <stdio.h>
-
-int sum(int a, int b) { return a + b; }
-
-void hello(const char* name) {
-  printf("Welcome %s this was built with scikit\n", name);
-}
--- src/hello.h
+++
@@ -1,7 +0,0 @@
-#ifndef HELLO_H
-#define HELLO_H
-
-int sum(int a, int b);
-void hello(const char* name);
-
-#endif
